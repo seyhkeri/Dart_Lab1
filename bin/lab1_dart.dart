c3d2dd188b789cf79a0475eefd9c9df64b7c2d68
seyhkeri <kerimovseyhan12@gmail.com>
2026-09-13 18:49:55 +0300

    learn dart syntax and types

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,5 +1,8 @@
 import 'package:lab1_dart/lab1_dart.dart' as lab1_dart;
 
-void main(List<String> arguments) {
-  print('Hello world: ${lab1_dart.calculate()}!');
+void main() {
+List<String> fruits2 = ['яблоко', 'банан', 'груша'];
+for (var fruit in fruits2) {
+  print(fruit);
+} 
 }
